cc450abfaeafa909a7d545e38dc6d6bffddd5bf3
jeevakalaiselvam <jeevak001@outlook.com>
2022-07-17 01:45:16 +0530

    API changed

--- helper/apiHelper.js
+++ helper/apiHelper.js
@@ -1,14 +1,14 @@
 export const getUrlPrefix = () => {
-  let PREFIX_URL = "";
+  let PREFIX_URL = '';
   switch (process.env.NODE_ENV) {
-    case "development":
-      PREFIX_URL = "http://localhost:3000/api";
+    case 'development':
+      PREFIX_URL = 'http://localhost:3000/api';
       break;
-    case "production":
-      PREFIX_URL = "http://app.com:3000/api";
+    case 'production':
+      PREFIX_URL = 'https://nextjs-steamtracker.vercel.app/api';
       break;
     default:
-      PREFIX_URL = "http://localhost:3000/api";
+      PREFIX_URL = 'http://localhost:3000/api';
       break;
   }
 
